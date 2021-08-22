2e12688625433accb8a1944ab29383440eb5e214
Christian Pschorr <c.pschorr@datamints.com>
2021-08-22 23:09:16 +0200

    Grundaufbau des Hero-Sliders definiert

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,7 +1,7 @@
 <?php
 
 // ---------------------------------------------------------------------------------------------------------------------
-// Icon Text Module von TT-Content
+// Hero
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
 	array(
 		'LLL:EXT:ce_hero/Resources/Private/Language/locallang_tca.xlf:tx_ce_hero.title',
@@ -37,14 +37,50 @@ $GLOBALS['TCA']['tt_content']['types']['ce_hero']['columnsOverrides']['image']['
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Hero Slider
-//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-//	array(
-//		'LLL:EXT:xo/Resources/Private/Language/locallang_tca.xlf:tx_xo_hero_slider.title',
-//		'xo_hero_slider',
-//		'content-textpic'
-//	),
-//	'CType',
-//	'xo_hero_slider'
-//);
-//
-//$GLOBALS['TCA']['tt_content']['types']['xo_hero_slider'] = $GLOBALS['TCA']['tt_content']['types']['xo_slider'];
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+	array(
+		'LLL:EXT:ce_hero/Resources/Private/Language/locallang_tca.xlf:tx_ce_hero_slider.title',
+		'ce_hero_slider',
+		'content-image'
+	),
+	'CType',
+	'ce_hero'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['ce_hero_slider'] = [
+	'showitem' => '
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.general;general,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.header;xoHeader, tx_xo_elements,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.appearance,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.frames;frames,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
+			--palette--;;hidden,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.visibility;visibility,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.access;access,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.extended
+	',
+];
+
+$GLOBALS['TCA']['tt_content']['types']['ce_hero_slider']['columnsOverrides']['tx_xo_elements']['config']['foreign_label'] = 'uid';
+$GLOBALS['TCA']['tt_content']['types']['ce_hero_slider']['columnsOverrides']['tx_xo_elements']['config']['overrideChildTca'] = [
+	'columns' => [
+		'record_type' => [
+			'config' => [
+				'items' => [
+					['LLL:EXT:ce_hero/Resources/Private/Language/locallang_tca.xlf:tx_xo_domain_model_elements.record_type.slider_image', 'ce_hero_slider_image'],
+				],
+				'default' => 'ce_hero_slider_image'
+			]
+		],
+
+	],
+	'types' => [
+		'ce_hero_slider_image' => [
+			'showitem' => '
+				l10n_diffsource, record_type, media,
+				--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
+				--palette--;;visibility,
+				--palette--;;access',
+		],
+	]
+];
